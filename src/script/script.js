b580028fc3002e3f6f2f69823e2ba294cf0f0ec4
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-06-27 03:00:16 -0400

    update login svg img and placeholder

--- src/script/script.js
+++ src/script/script.js
@@ -9,7 +9,9 @@ function checkLogin() {
     let regexPassword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-=_+{}[\]:;"'<>,.?/]).{8,16}$/;
 
     if (regexUsername.test(username.value) && regexPassword.test(password.value) == true) {
-        // do something here in future
+        // @todo: transition to main webpage showing classrooms && implement admin feature soon™
+        usernameLabel.classList.remove('input-error');
+        usernameLabel.classList.add('input-primary');
     } else if (regexUsername.test(username.value) == false && regexPassword.test(password.value) == false) {
 
         usernameLabel.classList.remove('input-primary');
@@ -17,6 +19,18 @@ function checkLogin() {
 
         passwordLabel.classList.remove('input-primary');
         passwordLabel.classList.add('input-error');
+
+        toolTip.classList.remove("hidden");
+
+    } else if (regexUsername.test(username.value) == true && regexPassword.test(password.value) == false) {
+        passwordLabel.classList.remove('input-primary');
+        passwordLabel.classList.add('input-error');
+
+        toolTip.classList.remove("hidden");
+    } else if (regexUsername.test(username.value) == false && regexPassword.test(password.value) == true) {
+
+        usernameLabel.classList.remove('input-primary');
+        usernameLabel.classList.add('input-error');
 
         toolTip.classList.remove("hidden");
 
