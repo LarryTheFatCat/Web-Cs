0989b0f33bd429ceb5603c8f97072ab9e0aa2b4d
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-07-01 19:25:37 -0400

    Rewrote authentication logic
    If any are wrong, return entire thing as wrong

--- src/script/validation.js
+++ src/script/validation.js
@@ -13,39 +13,29 @@ const userPasswordConfirmLabel = document.getElementById("password_label_confirm
 function checkLogin() {
     let regexUsername = /^[0-9]+$/;
     let regexPassword = /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
-
-    if (regexUsername.test(username.value) == true && regexPassword.test(password.value) == true) {
-        usernameLabel.classList.remove('input-error');
-        usernameLabel.classList.add('input-primary');
-
-        passwordLabel.classList.remove('input-error');
-        passwordLabel.classList.add('input-primary');
-        return location.href = "/src/classes.html"
-    } else if (regexUsername.test(username.value) == false && regexPassword.test(password.value) == false) {
-
-        usernameLabel.classList.remove('input-primary');
-        usernameLabel.classList.add('input-error');
-
-        passwordLabel.classList.remove('input-primary');
-        passwordLabel.classList.add('input-error');
-
+    // if any are wrong, return whole thing as wrong
+    // else, redirect to classes
+    if (!regexUsername.test(username.value)) {
+        usernameLabel.classList.remove("input-primary");
+        usernameLabel.classList.add("input-error");
+        passwordLabel.classList.remove("input-primary");
+        passwordLabel.classList.add("input-error");
         toolTip.classList.remove("hidden");
-
-    } else if (regexUsername.test(username.value) == true && regexPassword.test(password.value) == false) {
-        passwordLabel.classList.remove('input-primary');
-        passwordLabel.classList.add('input-error');
-
+        return;
+    } else if (!regexPassword.test(password.value)) {
+        usernameLabel.classList.remove("input-primary");
+        usernameLabel.classList.add("input-error");
+        passwordLabel.classList.remove("input-primary");
+        passwordLabel.classList.add("input-error");
         toolTip.classList.remove("hidden");
-    } else if (regexUsername.test(username.value) == false && regexPassword.test(password.value) == true) {
-
-        usernameLabel.classList.remove('input-primary');
-        usernameLabel.classList.add('input-error');
-
-        toolTip.classList.remove("hidden");
-    }
-
-    if(username.value == "TeacherAccessName" && password == "TeacherPassword") {
-        // toggle teacher admin section, mainly create classroom popup and some permissions
+        return;
+    } else {
+        usernameLabel.classList.remove("input-error");
+        usernameLabel.classList.add("input-primary");
+        passwordLabel.classList.remove("input-error");
+        passwordLabel.classList.add("input-primary");
+        toolTip.classList.add("hidden");
+        return location.href = "/src/classes.html";
     }
 }
 
@@ -53,49 +43,39 @@ function checkSignUp() {
     let regexUsername = /^[a-zA-Z0-9]+$/;
     let regexPassword = /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
     let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (!regexUsername.test(username.value) && regexPassword.test(password.value) == true) {
-        usernameLabel.classList.remove('input-error');
-        usernameLabel.classList.add('input-primary');
 
-        passwordLabel.classList.remove('input-error');
-        passwordLabel.classList.add('input-primary');
-        return false
-    }
+    // if any are wrong, return whole thing as wrong
+    // else, redirect to classes
+    if (!regexUsername.test(username.value)) {
+        usernameLabel.classList.remove("input-primary");
+        usernameLabel.classList.add("input-error");
+        passwordLabel.classList.remove("input-primary");
+        passwordLabel.classList.add("input-error");
+        return;
+    } else if (!regexPassword.test(password.value)) {
+        usernameLabel.classList.remove("input-primary");
+        usernameLabel.classList.add("input-error");
+        passwordLabel.classList.remove("input-primary");
+        passwordLabel.classList.add("input-error");
+        return;
+    } else if (!regexEmail.test(email.value)) {
+        usernameLabel.classList.remove("input-primary");
+        usernameLabel.classList.add("input-error");
+        passwordLabel.classList.remove("input-primary");
+        passwordLabel.classList.add("input-error");
+        emailLabel.classList.remove("input-primary");
+        emailLabel.classList.add("input-error");
+        return;
+    } else {
+        usernameLabel.classList.remove("input-error");
+        usernameLabel.classList.add("input-primary");
 
-    if (!regexUsername.test(username.value) && !regexPassword.test(password.value)) {
-        usernameLabel.classList.remove('input-primary');
-        usernameLabel.classList.add('input-error');
+        passwordLabel.classList.remove("input-error");
+        passwordLabel.classList.add("input-primary");
 
-        passwordLabel.classList.remove('input-primary');
-        passwordLabel.classList.add('input-error');
-        return false;
-    }
-    if (regexUsername.test(username.value) == true && !regexPassword.test(password.value)) {
-        passwordLabel.classList.remove('input-primary');
-        passwordLabel.classList.add('input-error');
-        return false;
-    }
-    if (!regexUsername.test(username.value) && regexPassword.test(password.value) == true) {
-        usernameLabel.classList.remove('input-primary');
-        usernameLabel.classList.add('input-error');
-        return false;
-    }
-    if(!regexEmail.test(email.value)) {
-        emailLabel.classList.remove('input-primary');
-        emailLabel.classList.add('input-error');
-        return false;
-    }
-    if(!regexPassword.test(userPasswordConfirm.value)) {
-        userPasswordConfirmLabel.classList.remove('input-primary');
-        userPasswordConfirmLabel.classList.add('input-error');
-        return false;
-    }
-    if(password.value != userPasswordConfirm.value) {
-        passwordLabel.classList.remove('input-primary');
-        passwordLabel.classList.add('input-error');
-        userPasswordConfirmLabel.classList.remove('input-primary');
-        userPasswordConfirmLabel.classList.add('input-error');
-        return false;
+        emailLabel.classList.remove("input-error")
+        emailLabel.classList.add("input-primary");
+        return location.href = "/src/classes.html"
     }
 }
 
