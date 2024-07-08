20aeb2c9ef00080fd6f05676308f482b11253310
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-07-07 21:36:31 -0400

    Add assignments card component section Rewrote assignment tab to include some basic features

--- src/script/PostAssignments.js
+++ src/script/PostAssignments.js
@@ -4,27 +4,40 @@ const assignmentContents = document.getElementById("assignment_contents");
 function createNewAssignment() {
     let textareaTitle = document.getElementById("textarea_title").value;
     let textareaDescription = document.getElementById("textarea_description").value;
+    let dueDate = document.getElementById("due_date").value.replace("T", " ");
+    console.log(dueDate)
 
-    if (textareaTitle == "" || textareaDescription == "") {
+    if (textareaTitle == "" || textareaDescription == "" || dueDate == "") {
         event.preventDefault();
         return false;
     } else {
     }
 
-    // assignment card details
     let card = document.createElement("div");
-    card.classList.add("card", "bg-base-100", "drop-shadow-2xl", "p-5", "cursor-pointer");
-    let textAreaTitleElement = document.createElement("p");
-    textAreaTitleElement.classList.add("join-item", "underline", "underline-offset-2", "cursor-pointer");
-    textAreaTitleElement.textContent = textareaTitle;
-    let assignmentIconSVG = document.createElement("img");
-    assignmentIconSVG.src = "../assets/assignmentCardIcon.svg";
-    assignmentIconSVG.classList.add("w-4", "h-4", "join-item");
+    card.classList.add("card", "bg-base-100", "transition", "drop-shadow-2xl", "w-full", "p-5", "border-l-gray-900", "border-l-4", "hover:cursor-pointer", "hover:bg-base-300");
+    
+    let cardContents = document.createElement("p");
+    cardContents.classList.add("card-title", "text-lg");
+    
+    let cardImageIcon = document.createElement("img");
+    cardImageIcon.src = "../assets/assignmentCardIcon.svg";
+    cardImageIcon.classList.add("w-10", "h-10", "bg-blue-700", "p-2", "rounded-full");
+
+    let cardTitleText = document.createElement("span");
+    cardTitleText.classList.add("overflow-hidden");
+    cardTitleText.textContent = textareaTitle;
+
+    let dueDateTag = document.createElement("p");
+    dueDateTag.textContent = "Due by: " + dueDate;
+    dueDateTag.classList.add("text-sm", "underline", "underline-offset-2", "text-right", "overflow-hidden");
+    
     // general assignments hierarchy
     assignmentContents.append(card);
-    card.appendChild(assignmentIconSVG);
-    card.appendChild(textAreaTitleElement);
-
+    card.appendChild(cardContents);
+    cardContents.appendChild(cardImageIcon);
+    cardContents.appendChild(cardTitleText);
+    cardContents.appendChild(dueDateTag)
+    
 
     card.addEventListener("click", function () {
         viewAssignment();
