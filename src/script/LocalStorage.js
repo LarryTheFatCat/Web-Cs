002576d065710bbeb256e2a75cd83fee8d94495d
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-05 14:52:43 -0400

    fixed bug with bg of empty classes being different than rest.
    Small fix lol

--- src/script/LocalStorage.js
+++ src/script/LocalStorage.js
@@ -1,10 +1,6 @@
 const darkModeCheckBox = document.getElementById("theme_switcher_checkbox");
 const isDark = JSON.parse(localStorage.getItem('isdark'));
 
-/**
- * Saves for theme only
- */
-
 darkModeCheckBox.checked = isDark;
 
 darkModeCheckBox.addEventListener('change', function() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-dark-thing': "#24282d"
+      }
+    },
   },
   plugins: [
     require('daisyui'),
