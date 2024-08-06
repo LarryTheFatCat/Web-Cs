01473c5a98c9981db87749102e3575a7201a406e
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-06 18:13:46 -0400

    Rebrand with NextJS Base

--- src/script/LocalStorage.js
+++
@@ -1,9 +0,0 @@
-const darkModeCheckBox = document.getElementById("theme_switcher_checkbox");
-const isDark = JSON.parse(localStorage.getItem('isdark'));
-
-darkModeCheckBox.checked = isDark;
-
-darkModeCheckBox.addEventListener('change', function() {
-  localStorage.setItem('isdark', JSON.stringify(darkModeCheckBox.checked));
-});
-
