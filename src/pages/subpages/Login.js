68a86d60652522c97876e8b2c9177a596800d7d5
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-08-06 22:56:31 -0400

    Navbar Implementation

---
+++ src/pages/subpages/Login.js
@@ -0,0 +1,9 @@
+import NavBar from "../components/NavBar"
+export default function Login() {
+    return (
+        <div>
+            <NavBar />
+            hello
+        </div>
+    )
+}
---
+++ src/pages/subpages/SignUp.js
@@ -0,0 +1,8 @@
+import NavBar from "../components/NavBar"
+export default function SignUp() {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    )
+}
